261b3281eaa8872abb4c136c25caffb746aa47fd
Michael Kishline <mkishline@carthage.edu>
2013-11-12 14:27:10 -0600

    massive changes to manager views; new template for edit view

--- djaludir/manager/urls.py
+++ djaludir/manager/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('djaludir.manager.views',
     url(r'^display/(?P<student_id>\d+)/$','display', name="alumna_display"),
     url(r'^search/$','search', name="manager_search"),
-    #url(r'^edit/(?P<student_id>\d+)/$','edit', name="user_edit"),
+    url(r'^edit/(?P<student_id>\d+)/$','edit', name="user_edit"),
 )

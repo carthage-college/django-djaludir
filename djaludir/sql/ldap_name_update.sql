28cd07db2314e8f39f306eb605f7d340c5ee19a8
Bender <plungerman@gmail.com>
2014-07-28 11:31:08 -0500

    update ldap name in informix

--- djaludir/sql/ldap_name_update.sql
+++ djaludir/sql/ldap_name_update.sql
@@ -1,1 +1,1 @@
-UPDATE cvid_rec SET ldap_name='michaelmarotz3081@gmail.com', ldap_add_date=TODAY WHERE cx_id = 34988
+UPDATE cvid_rec SET ldap_name='xxxxx@gmail.com', ldap_add_date=TODAY WHERE cx_id = xxxxx.

fe7ad76f52bd966d30d95a1d899891b52d0f9b42
plungerman <plungerman@gmail.com>
2019-02-01 10:06:22 -0600

    updates to wsgi default values for venv and requirements complete

--- djaludir/wsgi_default.py
+++ djaludir/wsgi_default.py
@@ -4,21 +4,15 @@ import traceback
 import signal
 import sys
 
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/usr/lib/python2.7/dist-packages/')
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-#sys.path.append('/data2/django_trunk/')
-#sys.path.append('/data2/django_1.5.5/')
-#sys.path.append('/data2/django_1.6/')
-#sys.path.append('/data2/django_1.8/')
-sys.path.append('/data2/django_1.11/')
-sys.path.append('/data2/django_projects/django-djaludir/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
-# Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'djaludir.settings'
-os.environ['PYTHON_EGG_CACHE'] = '/var/cache/python/.python-eggs'
-os.environ['TZ'] = 'America/Chicago'
+# python
+sys.path.append('/data2/python_venv/2.7/djaludir/lib/python2.7/')
+sys.path.append('/data2/python_venv/2.7/djaludir/lib/python2.7/site-packages/')
+sys.path.append('/data2/python_venv/2.7/djaludir/lib/django_projects/')
+sys.path.append('/data2/python_venv/2.7/djaludir/lib/django-djaludir/')
+# django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djaludir.settings')
+os.environ.setdefault('PYTHON_EGG_CACHE', '')
+os.environ.setdefault('TZ', 'America/Chicago')
 # informix
 os.environ['INFORMIXSERVER'] = ''
 os.environ['DBSERVERNAME'] = ''
@@ -27,14 +21,12 @@ os.environ['ODBCINI'] = ''
 os.environ['ONCONFIG'] = ''
 os.environ['INFORMIXSQLHOSTS'] = ''
 os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
-os.environ['LD_RUN_PATH'] = ''
+os.environ['LD_RUN_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules'
 # wsgi
 from django.core.wsgi import get_wsgi_application
 
-# for production:
-# uncomment out the following and comment out the try/except below
+# NOTE: remove the try/except in production
 #application = get_wsgi_application()
-
 try:
     application = get_wsgi_application()
 except Exception:
